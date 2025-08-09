be80dec5f294554985de45ab00e705bc7d366695
Rabble527 <pward527@live.com>
2025-08-08 22:10:27 -0600

    Add files via upload

---
+++ breadboard calculator.py
@@ -0,0 +1,38 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print("Welcome to the Breadboard Wire Measurement Tool\n")
+cross_gap = input("Will you cross the centre gap: \"y\" or \"n\"?")
+cross_rail = input("Will you connect to a power rail: \"y\" or \"n\"?")
+level_height = int(input("What height level? Note base = 0\n"))
+    
+number_of_bends = int(input("How many bends will there be?")) #subtract 0.05 inch per bend
+number_of_spaces = int(input("How many squares, including start and finish, will it cover?"))
+
+length_of_tail = float(input("How long will each tail be in mm?"))
+
+wire_length = float(0)
+total_length = float(0)
+
+wire_length += (number_of_spaces - 1) * 2.54
+
+wire_length -= (number_of_bends * (2.54 / 2))
+
+wire_length += (level_height  * 2.54)
+
+if cross_gap == "y":
+    wire_length += 5.08
+else:
+    wire_length += 0
+
+
+if cross_rail == "y":
+    wire_length += (7-2.54)
+else:
+    wire_length += 0
+
+
+total_length = round(wire_length + (length_of_tail * 2), 2)
+
+print("\n")
+print(f"You need to cut {total_length}mm")
